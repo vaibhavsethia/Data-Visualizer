0fc447a1b10b7102d5b8645ea024d2e081251737
Vaibhav Sethia <vaibhavsethia1998@gmail.com>
2020-01-08 00:48:50 +0530

    Updates

---
+++ precache-manifest.a6b2473580d0c68a638ddc9bd400df42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7fda160dbe5ea7374f1b969f6f735c",
+    "url": "/Data-Visualizer/index.html"
+  },
+  {
+    "revision": "bb2ccda857cfc9b16d1a",
+    "url": "/Data-Visualizer/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "49b12890d77aefd8b7cd",
+    "url": "/Data-Visualizer/static/css/main.9c83dace.chunk.css"
+  },
+  {
+    "revision": "bb2ccda857cfc9b16d1a",
+    "url": "/Data-Visualizer/static/js/2.03ab1cea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Data-Visualizer/static/js/2.03ab1cea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49b12890d77aefd8b7cd",
+    "url": "/Data-Visualizer/static/js/main.c17bf76a.chunk.js"
+  },
+  {
+    "revision": "da9cb68d3a981b0201f9",
+    "url": "/Data-Visualizer/static/js/runtime-main.fa2fd250.js"
+  }
+]);
